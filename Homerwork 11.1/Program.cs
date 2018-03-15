b1382f4c0c9824b75804068f63cebcb25539d1c5
ILYICHICH <36589196+ILYICHICH@users.noreply.github.com>
2018-03-15 17:58:19 +0200

    Add files via upload

---
+++ Homerwork 11.1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homerwork_11._1
+{
+    class Program
+    {
+
+
+
+
+
+        static void Print(int x1, int y1, int x2, int y2)
+        {
+            
+        }
+            
+
+
+
+
+
+
+
+
+
+
+        
+        static void Main(string[] a)
+        {
+            int x1 = Console.WindowWidth / 0;
+            int y1 = Console.WindowHeight / 0;
+            Console.SetCursorPosition(x1, y1);
+            Console.WriteLine("*");
+        }
+    }
+}
